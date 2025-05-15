d35c5df84b5c161a8d10366d08cf6e4a7c565b2d
njugus <kayvohnjuguna@gmail.com>
2025-05-14 22:48:33 -0700

    checks the user's password input 3 times then locks them if its wrong otherwise logs them in

--- Challenging Control Flow Questions.py
+++ Challenging Control Flow Questions.py
@@ -12,3 +12,20 @@ def leap_year_checker():
 
 
 print(leap_year_checker())
+
+# Password Checker
+# Ask the user for a password. Let them try up to 3 times.
+# Lock them out after 3 wrong tries.
+
+real_password = "tania"
+trial_count = 0
+
+while trial_count < 3:
+    password = str(input("Enter a password: "))
+    if password != real_password:
+        trial_count += 1
+        continue
+
+    print("Password correct!!")
+    break
+
--- practice.py
+++ practice.py
@@ -1,14 +1,19 @@
-# check whether a year is leap or not
-year = int(input("Enter a year: "))
+# Password Checker
+# Ask the user for a password. Let them try up to 3 times.
+# Lock them out after 3 wrong tries.
+
+real_password = "tania"
+trial_count = 0
+
+while trial_count < 3:
+    password = str(input("Enter a password: "))
+    if password != real_password:
+        trial_count += 1
+        continue
+
+    print("Password correct!!")
+    break
 
 
-# processing
-def leap_year_checker():
-    if year % 400 == 0 and year % 100 == 0:
-        return "Year is a leap year"
-    elif year % 4 == 0 and year % 100 != 0:
-        return "Year is a leap year"
-    return "Year is not a leap year"
 
 
-print(leap_year_checker())
